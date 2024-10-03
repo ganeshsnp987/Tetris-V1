15ef04a464df31a74506bd61d26fad1879f32dec
Ganesh Sanap <114701526+ganeshsnp987@users.noreply.github.com>
2024-10-03 11:14:46 +0530

    Update install_jenkins.sh

--- Jenkins-terraform/install_jenkins.sh
+++ Jenkins-terraform/install_jenkins.sh
@@ -19,6 +19,7 @@ sudo apt-get install docker.io -y
 sudo usermod -aG docker ubuntu  
 newgrp docker
 sudo chmod 777 /var/run/docker.sock
+#install sonarqube
 docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
 
 # install trivy
